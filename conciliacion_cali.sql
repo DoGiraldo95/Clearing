62495853e1c33c8b62bf264d91271c508f91e820
jabarrios375 <jbarrios@utryt.com.co>
2023-09-12 10:46:32 -0500

    consultas de consciliacion cali: red interna, bancolombia,banco de bogota

---
+++ conciliacion_cali.sql
@@ -0,0 +1,46 @@
+--Para conciliacion red interna Cali
+SELECT a.fecha_liq, SUM(a.MONTO) VALOR
+ FROM Mercury.Tbl_Valor_Consignar_Mrc a
+ WHERE a.fecha_liq >= TO_DATE('19-08-2023', 'DD-MM-YYYY') --fecha hoy
+ AND a.fecha_liq <= TO_DATE('22-08-2023', 'DD-MM-YYYY')
+ AND a.tp_id in (16,44)
+ GROUP BY a.fecha_liq;
+
+--Para conciliacion bancolombia
+SELECT
+  A.FECHA_LIQ,
+  CASE
+    WHEN A.TP_ID = 20 THEN
+      'BANCOLOMBIA'
+  END AS BANCO,
+  A.CANT_TRX,
+  A.MONTO
+FROM
+  MERCURY.TBL_VALOR_CONSIGNAR_MRC A
+WHERE
+  A.FECHA_LIQ >= TO_DATE('18-08-2023', 'DD-MM-YYYY') --Fecha de proceso
+  AND A.FECHA_LIQ <= TO_DATE('23-08-2023', 'DD-MM-YYYY') --Fecha de proceso
+  AND A.TP_ID = 20
+ORDER BY
+  FECHA_LIQ DESC;
+
+*
+/
+
+--Para conciliacion banco de bogota
+SELECT
+  A.FECHA_LIQ,
+  CASE
+    WHEN A.TP_ID = 19 THEN
+      'BANCO BOGOTA'
+  END AS BANBOGOTA,
+  A.CANT_TRX,
+  A.MONTO
+FROM
+  MERCURY.TBL_VALOR_CONSIGNAR_MRC A
+WHERE
+  A.FECHA_LIQ >= TO_DATE('18-08-2023', 'DD-MM-YYYY') --Fecha de proceso
+  AND A.FECHA_LIQ <= TO_DATE('23-08-2023', 'DD-MM-YYYY') --Fecha de proceso
+  AND A.TP_ID = 19
+ORDER BY
+  FECHA_LIQ DESC;
